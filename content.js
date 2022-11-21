1562a76998a4f4c2cf3ee33b74b45509b0425fe7
Riadh <riadh.azzoun@hotmail.com>
2022-11-21 02:40:38 +0100

    Refactoed code. Fixed a problem where the script wouldnt run if the user did not start in feed page.

--- content.js
+++ content.js
@@ -1,27 +1,36 @@
 // const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
-
-async function runApp() {
-  // Get timeline element
-  const timeline = await waitForTimeline();
-  const shadowParent = await waitForShadowParent();
-  // Observer
-  const config = { attributes: true, childList: true, subtree: true };
-  // Callback function to execute when mutations are observed
-  const callback = (mutationList, observer) => {
-    for (const mutation of mutationList) {
-      if (mutation.type === 'childList' & mutation.addedNodes.length > 0) {
-        for (element of mutation.addedNodes) {
-          // Check if added element is a post
-          try {
-            const dots = element.querySelector("[aria-haspopup='menu']");
-            if (!dots) return;
-            removeSponsor(element, shadowParent);
-          } catch (error) { return; }
-        }
+const DEBUG = true;
+// Get timeline element
+let timeline;
+let shadowParent;
+let currentUrl = location.href;
+const config = { attributes: true, childList: true, subtree: true };
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === 'childList' & mutation.addedNodes.length > 0) {
+      for (element of mutation.addedNodes) {
+        // Check if added element is a post
+        try {
+          const dots = element.querySelector("[aria-haspopup='menu']");
+          if (!dots) return;
+          removeSponsor(element, shadowParent);
+        } catch (error) { return; }
       }
     }
-  };
-  const observer = new MutationObserver(callback);
+  }
+};
+const observer = new MutationObserver(callback);
+
+async function getElements() {
+  timeline = await waitForTimeline();
+  shadowParent = await waitForShadowParent();
+}
+
+async function runApp() {
+  // Load Elements
+  await getElements()
+  // Start Observer
   observer.observe(timeline, config);
 }
 
@@ -40,9 +49,11 @@ async function removeSponsor(element, shadowParent) {
     if (x.textContent != 'Sponsored') continue;
     console.log(shadowID);
     // FOR DEBUG ONLY
-    // console.log(x.textContent);
-    // console.log(element);
-    // console.log("found the post");
+    if (DEBUG) {
+      console.log(x.textContent);
+      console.log(element);
+      console.log("found the post");
+    }
     x.remove();
     element.remove()
     break;
@@ -53,8 +64,13 @@ async function waitForShadowElements(shadowParent) {
   return new Promise((resolve, reject) => {
     const interval = setInterval(function () {
       const elements = shadowParent.querySelectorAll("[id*=gid]");
-
+      if (DEBUG) {
+        console.log('Searching for elements')
+      }
       if (elements) {
+        if (DEBUG) {
+          console.log('Found elements')
+        }
         clearInterval(interval);
         resolve(elements);
       }
@@ -68,6 +84,9 @@ async function waitForShadowParent() {
       const element = document.querySelector('div[style="position: absolute; top: -10000px;"]');
 
       if (element) {
+        if (DEBUG) {
+          console.log(`Found shadow parent ${element}`)
+        }
         clearInterval(interval);
         resolve(element);
       }
@@ -81,6 +100,9 @@ async function waitForTimeline() {
       const timeline = document.querySelector('[role="main"]');
 
       if (timeline) {
+        if (DEBUG) {
+          console.log(`Found timeline ${timeline}`)
+        }
         clearInterval(interval);
         resolve(timeline);
       }
@@ -88,5 +110,51 @@ async function waitForTimeline() {
   });
 }
 
+async function waitForBody() {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(function () {
+      const element = document.querySelector("body")
+
+      if (element) {
+        if (DEBUG) {
+          console.log(`Found body ${element}`)
+        }
+        clearInterval(interval);
+        resolve(element);
+      }
+    }, 50);
+  });
+}
+
+
+async function checkURL() {
+  const bodyList = await waitForBody();
+  const urlObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (currentUrl != document.location.href) {
+        currentUrl = document.location.href;
+        if(DEBUG){
+        console.log("url changed");
+        console.log(document.location.href);
+        }
+        if(document.location.href != "https://www.facebook.com/"){
+          // STOP OBSERVER
+          observer.disconnect();
+          return;
+        }
+        // RESTART THE APP
+        runApp();
+      }
+    });
+  });
+
+  var config = {
+    childList: true,
+    subtree: true
+  };
+
+  urlObserver.observe(bodyList, config);
+}
 
 runApp();
+checkURL();
